36607fe6a85b6e6624d4c52ce422b68cf95162e7
dheeraj-bhambhu <dhirajbhambhu18@gmail.com>
2026-06-19 22:58:15 +0530

    Implemented Expense CRUD APIs

---
+++ expense-tracker/src/main/java/com/dheeraj/expensetracker/controller/ExpenseController.java
@@ -0,0 +1,39 @@
+package com.dheeraj.expensetracker.controller;
+
+import com.dheeraj.expensetracker.entity.Expense;
+import com.dheeraj.expensetracker.service.ExpenseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/expenses")
+public class ExpenseController {
+    private final ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService){
+        this.expenseService = expenseService;
+    }
+    @PostMapping
+    public Expense createExpense(@RequestBody Expense expense){
+        return expenseService.addExpense(expense);
+    }
+    @GetMapping("/get")
+    public List<Expense> getExpense(){
+        return expenseService.getAllexpenses();
+    }
+    @GetMapping("/{id}")
+    public Expense getExpenseById(@PathVariable Long id){
+        return expenseService.getExpenseById(id);
+    }
+    @PutMapping("/{id}")
+    public Expense updateExpense(@PathVariable Long id,@RequestBody Expense expense){
+        return expenseService.updateExpense(id,expense);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteExpense(@PathVariable Long id){
+        expenseService.deleteExpense(id);
+    }
+
+
+}
---
+++ expense-tracker/src/main/java/com/dheeraj/expensetracker/service/ExpenseService.java
@@ -0,0 +1,41 @@
+package com.dheeraj.expensetracker.service;
+
+import com.dheeraj.expensetracker.entity.Expense;
+import com.dheeraj.expensetracker.repository.ExpenseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ExpenseService {
+
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseService(ExpenseRepository expenseRepository){
+        this.expenseRepository = expenseRepository;
+    }
+
+    public Expense addExpense(Expense expense){
+        return expenseRepository.save(expense);
+    }
+    public List<Expense> getAllexpenses(){
+        return expenseRepository.findAll();
+    }
+    public Expense getExpenseById(Long id){
+        return expenseRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Expense not found"));
+    }
+    public Expense updateExpense(Long id , Expense expense){
+        Expense existingExpense = expenseRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Expense not found"));
+                  existingExpense.setTitle(expense.getTitle());
+                  existingExpense.setAmount(expense.getAmount());
+
+                  return expenseRepository.save(existingExpense);
+    }
+    public void deleteExpense(Long id){
+        Expense Existingexpense = expenseRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Expense not found"));
+        expenseRepository.delete(Existingexpense);
+    }
+}
