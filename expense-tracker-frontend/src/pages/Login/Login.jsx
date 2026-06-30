44f529e7926a355c05149cb267380fdfe496d4f7
dheeraj-bhambhu <dhirajbhambhu18@gmail.com>
2026-06-30 23:23:28 +0530

    Day 2: Build login page UI with React state

--- expense-tracker-frontend/src/pages/Login/Login.jsx
+++ expense-tracker-frontend/src/pages/Login/Login.jsx
@@ -1,8 +1,55 @@
+import { useState } from "react";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="min-h-screen bg-gray-100 flex justify-center items-center">
       <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+        <h1 className="text-3xl font-bold text-center mb-6">
+          Expense Tracker
+        </h1>
 
+        <form>
+          {/* Email */}
+          <div className="mb-4">
+            <label className="block mb-2 font-medium">
+              Email
+            </label>
+
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-full border border-gray-300 rounded-lg p-3"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          {/* Password */}
+          <div className="mb-6">
+            <label className="block mb-2 font-medium">
+              Password
+            </label>
+
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="w-full border border-gray-300 rounded-lg p-3"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          {/* Login Button */}
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white p-3 rounded-lg"
+          >
+            Login
+          </button>
+        </form>
       </div>
     </div>
   );
